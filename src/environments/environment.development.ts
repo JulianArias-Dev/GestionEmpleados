7c84cf9142acd4adb654b1c550b3f843c589a3ce
JulianArias-Dev <davidariass0519@outlook.com>
2024-07-08 09:27:36 -0500

    Ajuste para despliegue

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+// src/environments/environment.ts
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBf-oPQLhmtX2TcE32C8O2-b37YuF2bxrQ",
+        authDomain: "empleados-1dc32.firebaseapp.com",
+        projectId: "empleados-1dc32",
+        storageBucket: "empleados-1dc32.appspot.com",
+        messagingSenderId: "676489750423",
+        appId: "1:676489750423:web:6a60fe6cf070adb81fc6d2"
+    },
+    production: true
+  };
